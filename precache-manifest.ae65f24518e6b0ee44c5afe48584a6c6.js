c5cce19b0507649197fc230c24a440a418f93daf
DmZheleznyak <11lione1991@gmail.com>
2020-06-07 20:56:19 +0300

    Updates

---
+++ precache-manifest.ae65f24518e6b0ee44c5afe48584a6c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "907e94a2560f39798db9",
+    "url": "/aviasales/static/js/runtime~main.f0e037fe.js"
+  },
+  {
+    "revision": "56838e9cc051b4cf3efd",
+    "url": "/aviasales/static/js/main.111644ad.chunk.js"
+  },
+  {
+    "revision": "6abd5a5586cb73ed23a2",
+    "url": "/aviasales/static/js/2.2362df50.chunk.js"
+  },
+  {
+    "revision": "56838e9cc051b4cf3efd",
+    "url": "/aviasales/static/css/main.1a702286.chunk.css"
+  },
+  {
+    "revision": "6c800b814788f9f9dc4f067c434842be",
+    "url": "/aviasales/index.html"
+  }
+];
--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-
-import { rootReducer } from './reducers/rootReducers';
-
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-const store = createStore( rootReducer, reduxDevTools )
-
-export default store;
